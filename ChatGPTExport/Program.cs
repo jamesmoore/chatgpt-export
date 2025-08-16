24a53e40bd4ca316eeede236b626536e75423dfb
jamesmoore <jamesmoore@whatever.com>
2025-08-16 17:34:44 +0100

    Detect OSC incompatible terminals

--- ChatGPTExport/Program.cs
+++ ChatGPTExport/Program.cs
@@ -6,12 +6,14 @@ using ChatGPTExport;
 using ChatGPTExport.Exporters;
 using ChatGPTExport.Models;
 
+var supportsOSCProgress = ConsoleFeatures.EnableOscTabProgress();
+
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 Console.CancelKeyPress += (sender, args) =>
 {
     args.Cancel = true;
-    Console.Write("\x1b]9;4;0;\x07");
+    TerminateProgram();
     Environment.Exit(0);
 };
 
@@ -73,71 +75,97 @@ var rootCommand = new RootCommand("ChatGPT export reformatter")
 
 rootCommand.SetAction(parseResult =>
 {
-    foreach (ParseError parseError in parseResult.Errors)
+    try
     {
-        Console.Error.WriteLine(parseError.Message);
-    }
-
-    Console.Write("\x1b]9;4;3\x07"); // https://learn.microsoft.com/en-us/windows/terminal/tutorials/progress-bar-sequences
-
-    var sourceDirectoryInfos = parseResult.GetRequiredValue(sourceDirectoryOption);
-    var fileSystem = new FileSystem();
-    var destination = fileSystem.DirectoryInfo.Wrap(parseResult.GetRequiredValue(destinationDirectoryOption));
-    var sources = sourceDirectoryInfos.Select(p => fileSystem.DirectoryInfo.Wrap(p));
-
-    var conversationFiles = sources.Select(p => p.GetFiles(searchPattern, SearchOption.AllDirectories)).SelectMany(s => s).ToList();
-
-    var conversationsFactory = new ConversationsParser(fileSystem);
-    var exporters = new List<IExporter>();
-    if (parseResult.GetRequiredValue(jsonOption))
-    {
-        exporters.Add(new JsonExporter(fileSystem, destination));
-    }
-    if (parseResult.GetRequiredValue(markdownOption))
-    {
-        exporters.Add(new MarkdownExporter(fileSystem, destination));
-    }
-    var exporter = new Exporter(fileSystem, exporters);
-
-    Conversations GetConversations(IFileInfo p)
-    {
-        try
+        foreach (ParseError parseError in parseResult.Errors)
         {
-            Console.WriteLine($"Loading conversation " + p.FullName);
-            return conversationsFactory.GetConversations(p);
+            Console.Error.WriteLine(parseError.Message);
         }
-        catch (Exception ex)
+
+        if (supportsOSCProgress)
         {
-            Console.Error.WriteLine($"Error parsing file: {p.FullName} {ex.Message}");
-            return null;
+            Console.Write("\x1b]9;4;3\x07"); // https://learn.microsoft.com/en-us/windows/terminal/tutorials/progress-bar-sequences
+        }
+
+        var sourceDirectoryInfos = parseResult.GetRequiredValue(sourceDirectoryOption);
+        var fileSystem = new FileSystem();
+        var destination = fileSystem.DirectoryInfo.Wrap(parseResult.GetRequiredValue(destinationDirectoryOption));
+        var sources = sourceDirectoryInfos.Select(p => fileSystem.DirectoryInfo.Wrap(p));
+
+        var conversationFiles = sources.Select(p => p.GetFiles(searchPattern, SearchOption.AllDirectories)).SelectMany(s => s).ToList();
+
+        var conversationsFactory = new ConversationsParser(fileSystem);
+        var exporters = new List<IExporter>();
+        if (parseResult.GetRequiredValue(jsonOption))
+        {
+            exporters.Add(new JsonExporter(fileSystem, destination));
+        }
+        if (parseResult.GetRequiredValue(markdownOption))
+        {
+            exporters.Add(new MarkdownExporter(fileSystem, destination));
+        }
+        var exporter = new Exporter(fileSystem, exporters);
+
+        Conversations GetConversations(IFileInfo p)
+        {
+            try
+            {
+                Console.WriteLine($"Loading conversation " + p.FullName);
+                return conversationsFactory.GetConversations(p);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Error parsing file: {p.FullName} {ex.Message}");
+                return null;
+            }
+        }
+
+        var directoryConversationsMap = conversationFiles
+            .Select(file => new
+            {
+                file.Directory,
+                Conversations = GetConversations(file)
+            })
+            .Where(x => x.Conversations != null)
+            .OrderBy(x => x.Conversations.GetUpdateTime())
+            .ToList();
+
+        var groupedByConversationId = directoryConversationsMap
+            .SelectMany(entry => entry.Conversations, (entry, Conversation) => (entry.Directory, Conversation))
+            .GroupBy(x => x.Conversation.conversation_id)
+            .OrderBy(p => p.Key).ToList();
+
+        var count = groupedByConversationId.Count;
+        var position = 0;
+        foreach (var group in groupedByConversationId)
+        {
+            if (supportsOSCProgress)
+            {
+                var percent = (int)(position++ * 100.0 / count);
+                Console.Write($"\x1b]9;4;1;{percent}\x07");
+            }
+            exporter.Process(group, destination);
         }
     }
-
-    var directoryConversationsMap = conversationFiles
-        .Select(file => new
-        {
-            file.Directory,
-            Conversations = GetConversations(file)
-        })
-        .Where(x => x.Conversations != null)
-        .OrderBy(x => x.Conversations.GetUpdateTime())
-        .ToList();
-
-    var groupedByConversationId = directoryConversationsMap
-        .SelectMany(entry => entry.Conversations, (entry, Conversation) => (entry.Directory, Conversation))
-        .GroupBy(x => x.Conversation.conversation_id)
-        .OrderBy(p => p.Key).ToList();
-
-    var count = groupedByConversationId.Count;
-    var position = 0;
-    foreach (var group in groupedByConversationId)
+    catch (Exception ex)
     {
-        var percent = (int)(position++ * 100.0 / count);
-        Console.Write($"\x1b]9;4;1;{percent}\x07");
-        exporter.Process(group, destination);
+        Console.Error.WriteLine(ex.Message);
+    }
+    finally
+    {
+        TerminateProgram();
     }
     return 0;
+
 });
 
 var parseResult = rootCommand.Parse(args);
 return parseResult.Invoke();
+
+void TerminateProgram()
+{
+    if (supportsOSCProgress)
+    {
+        Console.Write("\x1b]9;4;0;\x07");
+    }
+}
