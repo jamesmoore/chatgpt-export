344e0201eeb432e281ce288e1e1ec5565939ef21
jamesmoore <jamesmoore@github.com>
2026-01-28 08:04:47 +0000

    consolidate conversation finder

---
+++ ChatGPTExport/ConversationFinder.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.IO.Abstractions;
+
+namespace ChatGPTExport
+{
+    public class ConversationFinder
+    {
+        private const string SearchPattern = "conversations.json";
+
+        public IEnumerable<IFileInfo> GetConversationFiles(IEnumerable<IDirectoryInfo> sources)
+        {
+            var conversationFiles = sources.Select(GetConversationFiles).
+                SelectMany(fileInfo => fileInfo).ToList();
+            return conversationFiles;
+        }
+
+        public IEnumerable<IFileInfo> GetConversationFiles(IDirectoryInfo sourceDir)
+        {
+            return sourceDir.GetFiles(SearchPattern, SearchOption.AllDirectories);
+        }
+    }
+}
