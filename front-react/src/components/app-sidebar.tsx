5cda7d5b11c577dceb3fe4fe3fc1f77c4eff5ad0
James Moore <jamesmoore@users.noreply.github.com>
2026-02-06 09:31:34 +0000

    better loading and error messaging (#134)

--- front-react/src/components/app-sidebar.tsx
+++ front-react/src/components/app-sidebar.tsx
@@ -13,9 +13,7 @@ import {
 } from "@/components/ui/sidebar"
 import { useEffect } from "react";
 import { Link, useLocation, useParams } from "react-router-dom";
-
 import { useConversations } from '../hooks/use-conversations'
-import LoadingSpinner from "@/loading-spinner";
 
 export function AppSidebar() {
   const { id, format } = useParams();
@@ -23,17 +21,12 @@ export function AppSidebar() {
   const { setOpenMobile } = useSidebar();
 
   const {
-    data: conversations = [],
-    isLoading,
-    error,
+    data: conversations = []
   } = useConversations();
 
   useEffect(() => {
     setOpenMobile(false);
   }, [location.pathname, setOpenMobile]);
-
-  if (isLoading) return <LoadingSpinner />
-  if (error) return <div className="container"><p className="text-red-600">Error: {error.message}</p></div>
 
   return (
     <Sidebar className="h-full">
--- front-react/src/layout.tsx
+++ front-react/src/layout.tsx
@@ -3,6 +3,8 @@ import { AppSidebar } from './components/app-sidebar'
 import { SidebarProvider } from './components/ui/sidebar'
 import { DocumentTitle } from './document-title';
 import { useSystemThemeListener } from './hooks/use-system-theme-listener';
+import { useConversations } from './hooks/use-conversations';
+import LoadingSpinner from './loading-spinner';
 
 export interface LayoutProps {
   children?: ReactNode;
@@ -11,25 +13,46 @@ export interface LayoutProps {
 
 export default function Layout({ children, topBarChildren }: LayoutProps) {
   useSystemThemeListener();
+
+  const {
+    isLoading,
+    error,
+  } = useConversations();
+
   return (
     <>
       <DocumentTitle />
-      <SidebarProvider >
-        <div className="flex flex-1 h-screen overflow-hidden bg-neutral-100 dark:bg-neutral-900">
-          <AppSidebar />
-          <div className="flex-1 min-w-0 overflow-auto flex flex-col">
-            {topBarChildren &&
-              <div className="sticky top-0">
-                <div className="bg-sidebar p-1 pr-3">
-                  {topBarChildren}
-                </div>
-                <div className="shrink-0 h-px w-full bg-sidebar-border"></div>
-              </div>
-            }
-            {children}
-          </div>
+      {isLoading &&
+        <div className="flex h-screen">
+          <LoadingSpinner />
         </div>
-      </SidebarProvider >
+      }
+
+      {error && !isLoading &&
+        <div className="flex h-screen items-center justify-center text-red-600">
+          Error: {error.message}
+        </div>
+      }
+
+      {!isLoading && !error &&
+        <SidebarProvider >
+          <div className="flex flex-1 h-screen overflow-hidden bg-neutral-100 dark:bg-neutral-900">
+            <AppSidebar />
+            <div className="flex-1 min-w-0 overflow-auto flex flex-col">
+              {topBarChildren &&
+                <div className="sticky top-0">
+                  <div className="bg-sidebar p-1 pr-3">
+                    {topBarChildren}
+                  </div>
+                  <div className="shrink-0 h-px w-full bg-sidebar-border"></div>
+                </div>
+              }
+              {children}
+            </div>
+          </div>
+        </SidebarProvider >
+
+      }
     </>
   )
 }
